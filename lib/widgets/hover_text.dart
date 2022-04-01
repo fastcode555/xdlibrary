0cec73cefe0a34cfad526fc39a4c4ecb0a05c3ee
Barry <87644022+fastcode555@users.noreply.github.com>
2022-04-01 11:16:20 +0700

    [update]:为基类库增加HoverText的widget

---
+++ lib/widgets/hover_text.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/gestures/events.dart';
+
+/// @date 1/4/22
+/// describe:
+class HoverText extends StatefulWidget {
+  final TextStyle? style;
+  final String text;
+  final Color hoverColor;
+  final VoidCallback? onPressed;
+
+  const HoverText(
+    this.text, {
+    Key? key,
+    this.style,
+    this.hoverColor = Colors.red,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  _HoverTextState createState() => _HoverTextState();
+}
+
+class _HoverTextState extends State<HoverText> {
+  TextStyle? _hoverStyle;
+  TextStyle? _selectStyle;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectStyle = widget.style;
+    if (widget.style != null) {
+      _hoverStyle = widget.style!.copyWith(color: widget.hoverColor);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: _onEnter,
+      onExit: _onExit,
+      onHover: _onHover,
+      child: GestureDetector(
+        child: Text(widget.text, style: _selectStyle),
+        onTap: widget.onPressed,
+      ),
+    );
+  }
+
+  void _onEnter(PointerEnterEvent event) {}
+
+  void _onExit(PointerExitEvent event) {
+    setState(
+      () {
+        _selectStyle = widget.style;
+      },
+    );
+  }
+
+  void _onHover(PointerHoverEvent event) {
+    setState(() {
+      _selectStyle = _hoverStyle;
+    });
+  }
+}
