81e97f5cc0352f417427d3217c1a0b9c69ef1832
Barry <87644022+fastcode555@users.noreply.github.com>
2022-04-08 14:21:48 +0700

    [update]:add the round checkbox

--- lib/widgets/index.dart
+++ lib/widgets/index.dart
@@ -3,4 +3,5 @@ export 'package:xdlibrary/widgets/hover_text.dart';
 export 'package:xdlibrary/widgets/input_field.dart';
 export 'package:xdlibrary/widgets/pin_button.dart';
 export 'package:xdlibrary/widgets/pin_mark_widget.dart';
+export 'package:xdlibrary/widgets/round_checkbox.dart';
 export 'package:xdlibrary/widgets/test.dart';
