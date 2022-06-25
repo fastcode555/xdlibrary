5b5a426cdb358a3a0456edd76886bea93809c6a9
Barry <87644022+fastcode555@users.noreply.github.com>
2022-06-26 00:03:58 +0700

    删除Getx的引用

--- lib/widgets/pin_mark_widget.dart
+++ lib/widgets/pin_mark_widget.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
-
-void showSnackBar(String msg) {
-  ScaffoldMessenger.of(Get.context!).showSnackBar(SnackBar(content: new Text(msg)));
-}
 
 class PinSvg extends StatelessWidget {
   final String svg;
